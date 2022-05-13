65f3f5b8f9deb10bd65e6b90d44ab763c139294d
TitanBC <100634164+TitanBC@users.noreply.github.com>
2022-05-13 15:10:05 -0400

    Create stats.py

---
+++ stats.py
@@ -0,0 +1,13 @@
+weapon_choice = {'1':'sword', '2':'spear', '3':'greatsword'} 
+armor_choice = {'1':'leather armor', '2':'chain mail', '3':'plate armor'}
+
+monster_list = {'1':'Slime', '2':'Goblin', '3':'Troll'}
+monster_health = {'1':20, '2':40, '3':60}
+#1=sword, 2=spear, 3=greatsword
+
+sword = {'damage':8, }
+plate_armor = {'defense':3, 'speed':-1}
+
+weapon_list = {'sword and shield':{'damage':4, 'armor bonus':2}, 'spear':{'damage':6, 'armor bonus':0}, 'greatsword':{'damage':8, 'armor bonus':-1}}
+
+armor_list = {'leather armor':{'armor bonus':-1, 'speed bonus':3}, 'chain mail':{'armor bonus':1, 'speed bonus':1}, 'plate armor':{'armor bonus':3, 'speed bonus':-2} }
